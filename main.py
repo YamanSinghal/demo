01c2d0dfc1543fca239e07307bc0fe91086db709
YamanSinghal <yaman.singhal.yaman12@gmail.com>
2023-12-23 08:07:25 +0530

    fdg

--- main.py
+++ main.py
@@ -1,49 +1,41 @@
 import sys
+# sys.path.append("../")
 import streamlit as st
+# import pyaudio
+import wave
 from chatbot.audio_chat import AudioChatApp
 from chatbot.text_chat import TextChatApp
-
 # Initialize the AudioChatApp and TextChatApp
 audio_chat_app = AudioChatApp()
 text_chat_app = TextChatApp()
-# st.write("Secret Key", st.secrets["openai_secret_key"])
 
-print("main ",st.secrets["openai_secret_key"])
-# Set up Streamlit page configuration
+
+# FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = 1024
+RECORD_SECONDS = 5
+
 st.set_page_config(
     page_title="Strangify",
-    page_icon="strangify.jpeg"
+    page_icon="strangify.jpeg"  # Replace with the actual URL of your logo
 )
-
-# Set up Streamlit sidebar
-st.sidebar.header("Limitless support, One platform")
-st.sidebar.image("strangify.jpeg", use_column_width=True)
-st.sidebar.markdown("<small>Powered by Strangify</small>", unsafe_allow_html=True)
-
-# Initialize messages in session state if not already present
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Function to send a message
-def send_message():
-    input_message = st.session_state.input_message
-    response = text_chat_app.chat(input_message)
-    st.session_state.messages.append({"role": "strangify", "content": response})
-    
-    # Clear the input message after sending
-    st.session_state.input_message = ""
-
-# Display title
 st.title("Strangify")
 
 # Create the sidebar with settings
+st.sidebar.header("Limitless support , One platform")
+st.sidebar.image("strangify.jpeg", use_column_width=True)
+st.sidebar.markdown("<small>Powered by Strangify</small>", unsafe_allow_html=True)
 
-# Input for text messages
-st.text_input("What is up?", key="input_message", on_change=send_message)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# Button to send messages
+input_message = st.text_input("What is up?", key="input_message")
+recording = False
+
 if st.button("Send", key="send_button"):
-    send_message()
+    response = text_chat_app.chat(input_message)
+    st.session_state.messages.append({"role": "strangify", "content": response})
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -51,13 +43,3 @@ for message in st.session_state.messages:
         role = message['role']
         content = message['content']
         st.write(f"{role}: {content}")
-
-# JavaScript to simulate a click on the "Send" button when Enter is pressed
-js_code = """
-document.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        document.getElementById("send_button").click();
-    }
-});
-"""
-st.markdown(f"<script>{js_code}</script>", unsafe_allow_html=True)
